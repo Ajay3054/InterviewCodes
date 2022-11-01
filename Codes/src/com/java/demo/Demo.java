f1b6ef11ea9d02c800b5f2b050d3441d9de84ffa
user <user@DESKTOP-G9UGJ6B>
2022-11-01 17:50:50 +0530

    day

--- Codes/src/com/java/demo/Demo.java
+++ Codes/src/com/java/demo/Demo.java
@@ -3,7 +3,8 @@ package com.java.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("day");
+		System.out.println("day");
 
 	}
 
